5274e08a68703769f72616f5bd1afbe800b0fbe3
Repooc <tjcooper1982@gmail.com>
2021-06-08 20:46:59 -0400

    profiles and xml file

---
+++ ElvUI_DynamicStatusIcons/Settings/Global.lua
@@ -0,0 +1,7 @@
+local DSI, E, L, V, P, G = unpack(select(2, ...))
+
+G['DSI'] = {
+	defaultPacksPath = 'Interface\\AddOns\\ElvUI_DynamicStatusIcons\\Icons\\',
+	customDSIconsPath = 'Interface\\AddOns\\ElvUI_CustomDSIcons\\',
+	customizedOptions = {},
+}
---
+++ ElvUI_DynamicStatusIcons/Settings/Profile.lua
@@ -0,0 +1,20 @@
+local DSI, E, L, V, P, G = unpack(select(2, ...))
+
+local sharedOptions = {
+	enable = true,
+	iconpack = "Pepe - Pepe'jin",
+	anchorPoint = 'TOP',
+	xOffset = -3,
+	yOffset = 6,
+	scale = 1,
+	faceright = false,
+	color = {
+		r = 0.0117647058823529,
+		g = 0.5568627450980392,
+		b = 0.9843137254901961,
+		a = 1
+	},
+}
+
+P.unitframe.units.player.DynamicStatusIcons = sharedOptions
+P.unitframe.units.target.DynamicStatusIcons = sharedOptions
