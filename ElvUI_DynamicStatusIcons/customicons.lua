052179f9e8422316ea2608029992802357356720
Repooc <tjcooper1982@gmail.com>
2021-06-14 00:11:26 -0400

    have all custom icons that is not coming with the addon specifically here

---
+++ ElvUI_DynamicStatusIcons/customicons.lua
@@ -0,0 +1,110 @@
+local DSI = unpack(select(2, ...))
+
+DSI.customIcons = {
+	--! Additional Icons (Not Shipped w/ DSI)
+	--* Releaf
+	['Releaf - Pepe - Druid (Alliance)'] = {
+		pack = 'Releaf',
+		packtype = 'Alliance',
+		name = 'Druid (Alliance)',
+		additionalPack = true,
+	},
+	['Releaf - Pepe - Druid (Horde)'] = {
+		pack = 'Releaf',
+		packtype = 'Horde',
+		name = 'Druid (Horde)',
+		additionalPack = true,
+	},
+	['Releaf - Pepe - Mage (Horde)'] = {
+		pack = 'Releaf',
+		packtype = 'Horde',
+		name = 'Mage (Horde)',
+		additionalPack = true,
+	},
+	['Releaf - Pepe - Warlock (Alliance)'] = {
+		pack = 'Releaf',
+		packtype = 'Alliance',
+		name = 'Warlock (Alliance)',
+		additionalPack = true,
+	},
+	['Releaf - Pepe - Warrior (Alliance)'] = {
+		pack = 'Releaf',
+		packtype = 'Alliance',
+		name = 'Warrior (Alliance)',
+		additionalPack = true,
+	},
+	['Releaf - Pepe - Warrior (Horde)'] = {
+		pack = 'Releaf',
+		packtype = 'Horde',
+		name = 'Warrior (Horde)',
+		additionalPack = true,
+	},
+	--! Additional Icons (Not Shipped w/ DSI)
+	--* Heroes
+	['Pepe - Bolvar'] = {
+		pack = 'Heroes',
+		packtype = 'Pepe',
+		name = 'Bolvar',
+		additionalPack = true,
+	},
+	['Pepe - Elisande'] = {
+		pack = 'Heroes',
+		packtype = 'Pepe',
+		name = 'Elisande',
+		additionalPack = true,
+	},
+	["Pepe - Kael'Thas"] = {
+		pack = 'Heroes',
+		packtype = 'Pepe',
+		name = "Kael'Thas",
+		additionalPack = true,
+	},
+	['Pepe - Lich King'] = {
+		pack = 'Heroes',
+		packtype = 'Pepe',
+		name = 'Lich King',
+		additionalPack = true,
+	},
+	['Pepe - Ragnaros'] = {
+		pack = 'Heroes',
+		packtype = 'Pepe',
+		name = 'Ragnaros',
+		additionalPack = true,
+	},
+	['Pepe - Saurfang'] = {
+		pack = 'Heroes',
+		packtype = 'Pepe',
+		name = 'Saurfang',
+		additionalPack = true,
+	},
+	['Pepe - Sire Denathrius'] = {
+		pack = 'Heroes',
+		packtype = 'Pepe',
+		name = 'Sire Denathrius',
+		additionalPack = true,
+	},
+	['Pepe - Sylvanas'] = {
+		pack = 'Heroes',
+		packtype = 'Pepe',
+		name = 'Sylvanas',
+		additionalPack = true,
+	},
+	['Murloc - Demon Hunter'] = {
+		pack = 'Myshit',
+		packtype = 'Murlocs',
+		name = 'Demon Hunter',
+		additionalPack = true,
+	},
+	['Sadie - Pepe - Rogue (Horde)'] = {
+		pack = 'Sadie',
+		packtype = 'Horde',
+		name = 'Sadiemasokst',
+		additionalPack = true,
+	},
+	['Sadie - Pepe - Mistress'] = {
+		pack = 'Sadie',
+		packtype = 'BDSM',
+		name = 'Mistress Sadie',
+		additionalPack = true,
+	},
+}
