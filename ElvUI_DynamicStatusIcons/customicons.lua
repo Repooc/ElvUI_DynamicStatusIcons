083fa3b59148b300ebb787eae0daa88734019ac7
Repooc <tjcooper1982@gmail.com>
2021-06-27 12:55:13 -0400

    not in custom

--- ElvUI_DynamicStatusIcons/customicons.lua
+++ ElvUI_DynamicStatusIcons/customicons.lua
@@ -40,55 +40,7 @@ DSI.customIcons = {
 		additionalPack = true,
 	},
 	--! Additional Icons (Not Shipped w/ DSI)
-	--* Heroes
-	['Pepe - Heroes - Bolvar'] = {
-		pack = 'Heroes',
-		packtype = 'Pepe',
-		name = 'Bolvar Fordragon',
-		additionalPack = true,
-	},
-	['Pepe - Heroes - Elisande'] = {
-		pack = 'Heroes',
-		packtype = 'Pepe',
-		name = 'Elisande',
-		additionalPack = true,
-	},
-	["Pepe - Heroes - Kael'Thas"] = {
-		pack = 'Heroes',
-		packtype = 'Pepe',
-		name = "Kael'Thas Sunstrider",
-		additionalPack = true,
-	},
-	['Pepe - Heroes - Lich King'] = {
-		pack = 'Heroes',
-		packtype = 'Pepe',
-		name = 'the Lich King',
-		additionalPack = true,
-	},
-	['Pepe - Heroes - Ragnaros'] = {
-		pack = 'Heroes',
-		packtype = 'Pepe',
-		name = 'Ragnaros',
-		additionalPack = true,
-	},
-	['Pepe - Heroes - Saurfang'] = {
-		pack = 'Heroes',
-		packtype = 'Pepe',
-		name = 'Varok Saurfang',
-		additionalPack = true,
-	},
-	['Pepe - Heroes - Sire Denathrius'] = {
-		pack = 'Heroes',
-		packtype = 'Pepe',
-		name = 'Sire Denathrius',
-		additionalPack = true,
-	},
-	['Pepe - Heroes - Sylvanas'] = {
-		pack = 'Heroes',
-		packtype = 'Pepe',
-		name = 'Sylvanas Windrunner',
-		additionalPack = true,
-	},
+	--* Custom Work
 	['Sadie - Pepe - Rogue (Horde)'] = {
 		pack = 'Sadie',
 		packtype = 'Horde',
